2e40518c3cae794f26889686851e025a5806a9b7
Ronald_privat <ronald.fortmann@gmail.com>
2022-04-15 18:04:07 +0200

    ✨  Printing for each minute

--- faust_app.py
+++ faust_app.py
@@ -9,23 +9,29 @@ topic = app.topic("unique_users")
 
 @app.agent(topic)
 async def uniqueUsers(messages):
-    data_per_minute = []
+    data_per_minute = [{"ts": "", "uids": []}]
     async for event in messages:
-        # 1.2. Isolate uid and timestamp and unixstamp translate
         minute_event = {}
-        ts_minute = dt.utcfromtimestamp(event["ts"]).time().replace(second=0, microsecond=0)
-        if ts_minute not in [i for s in [d.keys() for d in data_per_minute] for i in s]:
-            minute_event[ts_minute] = []
-            minute_event[ts_minute].append(event["uid"])
+        ts_minute = (
+            dt.utcfromtimestamp(event["ts"])
+            .replace(second=0, microsecond=0)
+            .strftime("%Y/%m/%d %H:%M")
+        )
+
+        if ts_minute not in [d.get("ts") for d in data_per_minute]:
+            minute_event["ts"] = ts_minute
+            minute_event["uids"] = []
+            minute_event["uids"].append(event["uid"])
         else:
-            data_per_minute[data_per_minute.index(ts_minute)].append(event["uid"])
+            for value in data_per_minute:
+                if ts_minute == value.get("ts"):
+                    value["uids"].append(event["uid"])
         data_per_minute.append(minute_event)
         for minute in data_per_minute:
-            minute_key = list(minute)[0]
-            print(f"Unique visitors per minute at {minute_key}: {len(set(minute[minute_key]))}")
+            if minute.get("ts") is not None and minute.get("ts") != "":
+                print(f"Unique visitors per minute at {minute['ts']}: {len(set(minute['uids']))}")
 
 
-# 3. Write a method for unique values on a minute (optional: day, week, month and year)
 # 4. Push events in new topic
 
 if __name__ == "__main__":
