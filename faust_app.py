aa654d9faefc7fea0b4c0f0608f2c7effe48e021
Ronald_privat <ronald.fortmann@gmail.com>
2022-04-15 14:32:44 +0200

    🚧  uid and ts isolated

--- faust_app.py
+++ faust_app.py
@@ -7,8 +7,14 @@ topic = app.topic("unique_users")
 @app.agent(topic)
 async def uniqueUsers(messages):
     async for event in messages:
-        print(event)
+        # 1. Isolate uid and timestamp
+        print(event["uid"])
+        print(event["ts"])
 
+
+# 2. Translate Unix timestamp into normal date
+# 3. Write a method for unique values on a minute (optional: day, week, month and year)
+# 4. Push events in new topic
 
 if __name__ == "__main__":
     app.main()
